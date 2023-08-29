17814dce09a5f8693ab2cc47a6ab68a3b5deb233
Dipaknikalje <dipaknikalje80@gmail.com>
2023-08-29 11:37:48 +0530

    update

--- db/mongooseDb.js
+++ db/mongooseDb.js
@@ -1,16 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-// const url="mongodb+srv://techdips:rushikesh@cluster0.npv63ry.mongodb.net/?retryWrites=true&w=majority"
-const url="mongodb://127.0.0.1:27017/ecomapp"
+const url =
+  "mongodb+srv://techdips:rushikesh@cluster0.npv63ry.mongodb.net/?retryWrites=true&w=majority";
+// const url="mongodb://127.0.0.1:27017/ecomapp"
 
-const connect= async ()=>{
-    try {
-        console.log("trying to conect")
-        await mongoose.connect(url);
-        console.log("db is connected")
-    } catch (error) {
-        console.log(error.message,"due to this occured")
-    }
-}
+const connect = async () => {
+  try {
+    console.log("trying to conect");
+    await mongoose.connect(url);
+    console.log("db is connected");
+  } catch (error) {
+    console.log(error.message, "due to this occured");
+  }
+};
 
-module.exports=connect;
+module.exports = connect;
